ac3a957ceb2daa75c1c8efe9cf1ac220ea0b877b
Ramiro Fryda <L0796212@gal089388.bgcmz.bancogalicia.com.ar>
2016-06-01 23:06:03 -0300

    Quicksort andando

--- src/test/TestQuickSort.java
+++ src/test/TestQuickSort.java
@@ -1,9 +1,30 @@
 package test;
 
+import java.util.Arrays;
+
+import dyc.QuickSort;
+
 public class TestQuickSort {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+
+		QuickSort q = new QuickSort();
+		
+		int[] valores = { 3, 8, 7, 5, 2, 1, 9, 6, 4 };
+        int longitud = 9;
+						
+		System.out.println("Valores sin ordenar");
+		System.out.println(Arrays.toString(valores));
+		
+		try {
+			q.quickSort(valores, 0, longitud-1);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}					
+		
+		System.out.println();
+		System.out.println("Valores ordenado");
+		System.out.println(Arrays.toString(valores));
 
 	}
 
