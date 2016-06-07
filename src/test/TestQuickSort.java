97be8f5c9b0eaa5c265c84985d8790e2eb707c49
Bruno Guilianetti <bgiulianetti@competir.com>
2016-06-07 12:25:57 -0300

    correccion de reemplazo de archivo repetido

--- src/test/TestQuickSort.java
+++ src/test/TestQuickSort.java
@@ -12,15 +12,14 @@ public class TestQuickSort {
 		
 		System.out.println("QuickSort");
 		System.out.println();
-		
-		int[] valores = { 3, 8, 7, 5, 2, 1, 9, 6, 4 };
-        int longitud = 9;
+		System.out.println("caso 1");
+		int[] valores = { 3, 9, 7, 6, 2, 1, 8, 5, 4 };
 						
 		System.out.println("Valores sin ordenar");
 		System.out.println(Arrays.toString(valores));
 		
 		try {
-			q.quickSort(valores, 0, longitud-1);
+			q.quickSort(valores);
 		} catch (Exception e) {
 			e.printStackTrace();
 		}					
@@ -28,6 +27,23 @@ public class TestQuickSort {
 		System.out.println();
 		System.out.println("Valores ordenados por QuickSort");
 		System.out.println(Arrays.toString(valores));
+		
+		System.out.println();
+		System.out.println("caso 2");
+		int[] valores1 = { 3, 9, 17, 6, 12, 1, 18, 5, 4, 2, 10, 7, 11, 19, 8, 20, 13, 16  };
+						
+		System.out.println("Valores sin ordenar");
+		System.out.println(Arrays.toString(valores1));
+		
+		try {
+			q.quickSort(valores1);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}					
+		
+		System.out.println();
+		System.out.println("Valores ordenados por QuickSort");
+		System.out.println(Arrays.toString(valores1));
 
 	}
 
